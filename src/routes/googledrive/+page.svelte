2bb6a37702ef1e46f8b2d5e7dcff25f4141bcc12
Snazzah <me@snazzah.com>
2024-04-18 14:21:52 -0500

    fix: prompt google drive less if auth is still fine

--- src/routes/googledrive/+page.svelte
+++ src/routes/googledrive/+page.svelte
@@ -11,6 +11,7 @@
 	let pickerInited = false;
 	let redirecting = false;
 	const isolated = window.crossOriginIsolated;
+	const probablyExpired = !$googleDriveData || (($googleDriveData.created + ($googleDriveData.expires_in * 1000)) < Date.now());
 
 	function gisLoaded() {
 		tokenClient = window.google.accounts.oauth2.initTokenClient({
@@ -21,7 +22,7 @@
 		gisInited = true;
 	}
 
-	function createPicker() {
+	function createPicker(forcePrompt = false) {
 		const showPicker = () => {
 			const view = new window.google.picker.View(window.google.picker.ViewId.DOCS);
 			view.setMimeTypes(ALLOWED_TYPES.join(','));
@@ -46,7 +47,8 @@
 			console.log('Token client error', error.message, { error });
 		};
 
-		tokenClient.requestAccessToken({ prompt: !$googleDriveData ? 'consent' : '' });
+		if (forcePrompt || probablyExpired) tokenClient.requestAccessToken({ prompt: !$googleDriveData ? 'consent' : '' });
+		else showPicker();
 	}
 
 	// A simple callback implementation.
@@ -89,9 +91,14 @@
 	{:else if redirecting}
 		<span>Redirecting you back to the app...</span>
 	{:else}
-		<button on:click={createPicker} class="rounded-md px-4 py-2 transition-all bg-violet-700 text-white hover:bg-violet-600 active:scale-95">
+		<button on:click={() => createPicker()} class="rounded-md px-4 py-2 transition-all bg-violet-700 text-white hover:bg-violet-600 active:scale-95">
 			Pick a File
 		</button>
-		<a href="/" class="hover:underline text-sm mt-4">back</a>
+		{#if !probablyExpired}
+			<button on:click={() => createPicker(true)} class="hover:underline text-sm mt-1">
+				or choose a different account
+			</button>
+		{/if}
+		<a href="/" class="transition-all text-violet-500 hover:text-violet-400 mt-4">&lt;- back</a>
 	{/if}
 </main>
