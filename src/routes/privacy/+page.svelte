a71222510591bdccb98e8ad166023e2716fc2272
Snazzah <me@snazzah.com>
2024-05-14 20:20:17 -0500

    chore: last update to the privacy policy before i give up on google

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -21,7 +21,7 @@
 			use of any third-party service (like Dropbox, Google Drive, etc.) will only be used to retrieve the requested file by the user to be downloaded into webapp from the browser.
 			when authenticating with Google, an access token is retrieved and is used to download requested files (via Google File Picker) from that service.
 			authorization data and access tokens are stored in the browser (using local storage) and is never sent to any third-party server.
-			authorization data and access tokens stored using catcut is secure and only accessible through the catcut app.
+			authorization data and access tokens stored using catcut is secure and only accessible through the catcut app. security procedures are in place to protect the confidentiality of your data.
 		</p>
 		{#if PUBLIC_PLAUSIBLE_HOSTNAME}
 			<br />
