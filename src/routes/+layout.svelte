91d9bb42826159ba8acd7f7e9f03a0404722059c
Snazzah <me@snazzah.com>
2026-09-16 11:27:49 -0400

    chore: actually fix the mobile screen issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,7 +16,7 @@
 	{/if}
 </svelte:head>
 
-<div class="min-h-screen min-w-80 font-sans text-neutral-400 scheme-dark">
+<div class="min-h-svh min-w-80 font-sans text-neutral-400 scheme-dark">
 	<div class="fixed -z-1 bg-linear-to-b from-transparent via-violet-300/2 to-violet-500/5 top-0 left-0 right-0 h-svh"></div>
 	<Tooltip.Provider>
 		{@render children()}
